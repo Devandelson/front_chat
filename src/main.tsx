d9479e5b3d91fc9e2f8453b74680b2ce2ec85898
Devandelson <devandelsongonzalez@gmail.com>
2026-07-16 14:51:43 -0400

    fix route 2

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/front_chat">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
