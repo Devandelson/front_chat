282e9edb52a468a16c33bd89e8e574b8832814c3
Devandelson <devandelsongonzalez@gmail.com>
2026-07-16 14:34:11 -0400

    route

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/front_chat">
       <App />
     </BrowserRouter>
   </StrictMode>,
