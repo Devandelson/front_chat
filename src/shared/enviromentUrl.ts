b7c22715cece44b852221e070f85401fdf299175
Devandelson <devandelsongonzalez@gmail.com>
2026-07-16 15:40:47 -0400

    fix routes

--- src/shared/enviromentUrl.ts
+++ src/shared/enviromentUrl.ts
@@ -1,2 +1,3 @@
-const route = 'https://backend-onlinechat.onrender.com/'; 
+// const route = 'https://backend-onlinechat.onrender.com/'; 
+const route = 'http://localhost:3000/'; 
 export default route;
