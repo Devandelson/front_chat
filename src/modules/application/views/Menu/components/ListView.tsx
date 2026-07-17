f6469c59da86fa14c2ceb929ec49ac6dd921e324
Devandelson <devandelsongonzalez@gmail.com>
2026-07-17 00:15:42 -0400

    fix responsive desing

--- src/modules/application/views/Menu/components/ListView.tsx
+++ src/modules/application/views/Menu/components/ListView.tsx
@@ -24,13 +24,14 @@ export default function Li({ name, actives, state, setChat }: LiProps) {
 
     return (
         <li className={`flex items-center gap-2.5 bg-gray-500/30 rounded-sm p-2 cursor-pointer
-        hover:-translate-y-1.5 hover:bg-gray-900/80 transition-all duration-150 ${active}`}
+        hover:-translate-y-1.5 hover:bg-gray-900/80 transition-all duration-150 ${active}
+        `}
         onClick={() => {SwitchChat(name)}}
         >
             <span className="w-9 aspect-square rounded-full bg-blue-300 text-white
             text-center flex items-center justify-center">{firstLetter}</span>
             <div className="text-white">
-                <p>{name}</p>
+                <p className='w-full wrap-break-word text-wrap'>{name}</p>
                 <p>Activos: {actives}</p>
             </div>
         </li>
--- src/modules/application/views/Menu/menu.tsx
+++ src/modules/application/views/Menu/menu.tsx
@@ -3,6 +3,8 @@ import { motion, type Variants } from "motion/react";
 import { type ChatProps } from '../../utils/infoChat.tsx';
 import { useNavigate } from "react-router";
 import Swal from "sweetalert2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 // components =============================================
 import Header from "./components/header.tsx";
@@ -11,6 +13,9 @@ import ListView from './components/ListView.tsx';
 
 // Shared =============================================
 import { Button1 } from '@shared/smallComponent.tsx';
+
+// hooks =============================================
+import { useState } from "react";
 
 
 interface MenuProps {
@@ -58,30 +63,46 @@ function Menu({ chat, setChat, users }: MenuProps) {
     }
 
     const styleRole = Role == 'admin' ? 'grid-rows-[repeat(4,auto)_1fr_auto]'
-    : 'grid-rows-[repeat(3,auto)_1fr_auto]';
+        : 'grid-rows-[repeat(3,auto)_1fr_auto]';
     const activeUsers = users.length;
 
+    const [visibleMenu, setVisibleMenu] = useState(true);
+    const styleMenu = visibleMenu ? 'max-md:max-w-0!' : 'max-w-87.5';
+    const styleIcon = visibleMenu ? 'bg-red-950 -right-15' : 'bg-blue-950 right-5';
+
     return (
-        <motion.div className={`h-screen overflow-y-auto relative p-4 bg-gray-700
-        grid ${styleRole}`}
+        <motion.div className={`h-screen relative bg-gray-700 w-full ${styleMenu} transition-all`}
             variants={menuVariants}
             initial="hidden"
             animate="visible"
         >
-            <Header name={userName}></Header>
-            <hr className="border border-white/20" />
-            { Role == 'admin' && (<Controls chats={chat} setChats={setChat}></Controls>) }
-            <hr className="border border-white/20" />
+            <button className={`p-2 text-white absolute top-6 rounded-sm z-10
+                hover:scale-110 active:scale-[0.95] transition-all duration-200 ${styleIcon}
 
-            <ul className="mt-4 w-full relative flex flex-col gap-3">
-                {
-                    chat.map((item, index) => (
-                        <ListView key={index} name={item.name} state={item.state} actives={activeUsers} setChat={setChat}></ListView>
-                    ))
-                }
-            </ul>
+                hidden max-md:block  
+            `}
+                onClick={() => { setVisibleMenu((prev) => !prev) }}
+            >
+                <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
+            </button>
 
-            <Button1 title="Cerrar sesión" style="w-full! text-white! cursor-pointer! hover:scale-110! h-max!" onClick={() => { handleSingOut() }} />
+            <div className={`h-screen overflow-y-auto relative bg-gray-700
+            grid ${styleRole} w-full ${visibleMenu ? 'p-4 max-md:p-0' : 'p-4'}  transition-all`}>
+                <Header name={userName}></Header>
+                <hr className="border border-white/20" />
+                {Role == 'admin' && (<Controls chats={chat} setChats={setChat}></Controls>)}
+                <hr className="border border-white/20" />
+
+                <ul className="mt-4 w-full relative flex flex-col gap-3">
+                    {
+                        chat.map((item, index) => (
+                            <ListView key={index} name={item.name} state={item.state} actives={activeUsers} setChat={setChat}></ListView>
+                        ))
+                    }
+                </ul>
+
+                <Button1 title="Cerrar sesión" style="w-full! text-white! cursor-pointer! hover:scale-110! h-max!" onClick={() => { handleSingOut() }} />
+            </div>
         </motion.div>
     )
 }
